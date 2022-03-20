04837247fd5bcbd6f862e76afecea9c09a9dceb6
SomeshBanna <someshbanna845@gmail.com>
2022-03-20 16:49:09 +0530

    Logger added in test

--- src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
+++ src/test/java/net/javaguides/springboot/SpringbootBackendApplicationTests.java
@@ -1,13 +1,25 @@
 package net.javaguides.springboot;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class SpringbootBackendApplicationTests {
 
+	public static Logger logger=LoggerFactory.getLogger(SpringbootBackendApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("TEST CASE EXECUTING...");
+		assertEquals(true,true);
+		
 	}
+
 
 }
